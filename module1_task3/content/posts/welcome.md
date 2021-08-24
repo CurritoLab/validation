5b512872694d788172d3d5dea439e2d4dfe90b10
Curro <curritolab@gmail.com>
2021-08-24 16:53:01 +0200

    Modulo1 task3.0

---
+++ module1_task3/content/posts/welcome.md
@@ -0,0 +1,6 @@
+---
+title: "Welcome to Awesome Inc."
+date: 2021-08-16T00:49:21+02:00
+draft: true
+---
+
---
+++ module1_task3/setup.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+# instalamos hugo y make
+apt-get update && apt-get install -y hugo make
+
+# ejecutamos el build
+make build
